20a3886d54ee3d231726d9bb426d0b5e2f8f2176
Vusal <vusald79@gmail.com>
2023-03-28 06:52:23 +0400

    js-task

---
+++ script.js
@@ -0,0 +1,9 @@
+let num = prompt("Enter a number: ");
+
+if (num > 0) {
+  console.log(num + " is a positive number");
+} else if (num < 0) {
+  console.log(num + " is a negative number");
+} else {
+  console.log("The entered number is zero.");
+}
